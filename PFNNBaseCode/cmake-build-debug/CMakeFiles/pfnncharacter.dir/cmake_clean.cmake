8df95efd1cd89289abda3991e5f9ff29235f317c
MariaPriisalu <maria.priisalu@gmail.com>
2021-11-26 16:26:13 +0100

    Added teh colmap files

---
+++ PFNNBaseCode/cmake-build-debug/CMakeFiles/pfnncharacter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pfnncharacter.dir/PFNNBaseCode.cpp.o"
+  "CMakeFiles/pfnncharacter.dir/main.cpp.o"
+  "pfnncharacter.cpython-38-x86_64-linux-gnu.so"
+  "pfnncharacter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pfnncharacter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PFNNBaseCode/cmake-build-debug/glm/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: Work/RLAgent/commonUtils/PFNNBaseCode/glm/glm
+# Build directory: Work/RLAgent/commonUtils/PFNNBaseCode/cmake-build-debug/glm/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
